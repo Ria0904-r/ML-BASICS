de3439149041ff7a2dc2b83161d76837d7c1c933
Ria Sehgal <riasehgal48@gmail.com>
2021-10-08 20:46:19 +0530

    Add files via upload

---
+++ Untitled52.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas
+
+
+# In[3]:
+
+
+df=pandas.read_csv("Housing Prices.csv")
+
+
+# In[4]:
+
+
+df
+
+
+# In[5]:
+
+
+df.dtypes
+#to find data types of variable
+
+
+# In[6]:
+
+
+df.head(10)
+#to display top 10 rows 
+
+
+# In[7]:
+
+
+df.tail(10)
+#to display last 10 rows of data set
+
+
+# In[ ]:
+
+
+
+
